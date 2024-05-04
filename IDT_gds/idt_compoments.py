e603b8990c8649222a9b9f97025c3e3c11ce07d3
haoqindeng-uw <haoqinde@uw.edu>
2024-05-03 17:08:27 -0700

    new idt

---
+++ IDT_gds/idt_compoments.py
@@ -0,0 +1,21 @@
+import numpy as np
+from math import pi
+from gdshelpers.geometry.chip import Cell
+from gdshelpers.parts.waveguide import Waveguide
+from gdshelpers.parts.coupler import GratingCoupler
+from gdshelpers.parts.resonator import RingResonator
+from gdshelpers.layout import GridLayout
+from gdshelpers.parts.marker import CrossMarker
+from gdshelpers.parts.marker import SquareMarker
+from gdshelpers.helpers.positive_resist import convert_to_positive_resist
+from gdshelpers.parts.port import Port
+from shapely.geometry import Polygon
+from shapely.affinity import scale
+from shapely.affinity import translate
+from gdshelpers.geometry import geometric_union
+from gdshelpers.helpers.under_etching import create_holes_for_under_etching
+from gdshelpers.geometry.ebl_frame_generators import raith_marker_frame
+from gdshelpers.parts.text import Text
+
+def make_single_idt(pos=[0,0], zno=False, orientation='right', aperture=15, pair_num=50, pitch=0.1, bias=0.005):
+    
